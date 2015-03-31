eb2c84235932a440e2575b94a45d5d273da79a5d
darumacity <lt012071@gmail.com>
2015-03-31 20:58:01 +0900

    [modify]Define Models of Entry

--- entry/admin.py
+++ entry/admin.py
@@ -1,3 +1,9 @@
+# coding: utf-8
+
 from django.contrib import admin
+from entry.models import Tag, Entry
 
 # Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Entry)
--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,23 @@
+# coding: utf-8
+
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+class Entry(models.Model):
+    from os import path
+    
+    title = models.CharField(max_length=1024)
+    path = models.FilePathField(path=path.join(path.abspath(path.dirname(__file__)), 'templates/entry/entries'), recursive=True)
+    post_user = models.CharField(max_length=30)
+    post_date_time = models.DateTimeField()
+    tags = models.ManyToManyField(Tag)
+
+    def __unicode__(self):
+        return self.title
