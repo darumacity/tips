b07123ae4d2a155f5df3f7c6779fe001d488077c
darumacity <lt012071@gmail.com>
2015-05-25 22:15:27 +0900

    [modify]Add archive of tag

--- entry/views.py
+++ entry/views.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from django.shortcuts import render_to_response
-from entry.models import Entry
+from entry.models import Tag, Entry
 
 # Create your views here.
 
@@ -13,13 +13,16 @@ def media(request, media_path):
     return serve(request, media_path, media_root)
 
 def top(request):
-    return render_to_response('entry/top.html',
-                              {'entries': Entry.objects.all(),
-                               'recent_entries': Entry.objects.all()})
+    return render_to_response('entry/archive.html',
+                              {'archive_title': "最近の記事",
+                               'entries': Entry.objects.all(),
+                               'recent_entries': Entry.objects.all(),
+                               'tags': Tag.objects.all()})
 
 def content(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
-    return render_to_response(entry.path, {'recent_entries': Entry.objects.all()})
+    return render_to_response(entry.path, {'recent_entries': Entry.objects.all(),
+                                           'tags': Tag.objects.all()})
 
 def resource(request, entry_id, resource_path):
     from django.views.static import serve
@@ -27,3 +30,11 @@ def resource(request, entry_id, resource_path):
 
     entry = Entry.objects.get(id=entry_id)
     return serve(request, resource_path, path.dirname(entry.path))
+
+def tag(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    return render_to_response('entry/archive.html',
+                              {'archive_title': tag.name,
+                               'entries': Entry.objects.filter(tags__id=tag_id),
+                               'recent_entries': Entry.objects.all(),
+                               'tags': Tag.objects.all()})
