b281d8bafe7a75cf5b4235df1677218a5702550f
darumacity <lt012071@gmail.com>
2015-05-25 22:33:54 +0900

    [fix]Change to refer to media files using the built-in template tag 'url'

--- entry/urls.py
+++ entry/urls.py
@@ -4,7 +4,7 @@ from entry import views
 
 urlpatterns = patterns('',
     url(r'^$', views.top),
-    url(r'^media/(?P<media_path>.+)/$', views.media),
+    url(r'^media/(?P<media_path>.+)/$', views.media, name='media'),
     url(r'^(?P<entry_id>\d+)/$', views.content, name='content'),
     url(r'^(?P<entry_id>\d+)/(?P<resource_path>.+)/$', views.resource),
     url(r'^tag/(?P<tag_id>\d+)/$', views.tag, name='tag'),
