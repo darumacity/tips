526ee95d0152938f8eafba8de373d33136322bfc
darumacity <lt012071@gmail.com>
2015-05-27 22:19:45 +0900

    [modify]Move the reference definition to the top

--- entry/views.py
+++ entry/views.py
@@ -1,14 +1,13 @@
 # coding: utf-8
 
 from django.shortcuts import render_to_response
+from django.views.static import serve
+from os import path
 from entry.models import Tag, Entry
 
 # Create your views here.
 
 def media(request, media_path):
-    from django.views.static import serve
-    from os import path
-
     media_root = path.join(path.abspath(path.dirname(__file__)), 'templates/entry/media')
     return serve(request, media_path, media_root)
 
@@ -23,9 +22,6 @@ def content(request, entry_id):
     return render_to_response(entry.path, {'sidebar_info': SidebarInfo})
 
 def resource(request, entry_id, resource_path):
-    from django.views.static import serve
-    from os import path
-
     entry = Entry.objects.get(id=entry_id)
     return serve(request, resource_path, path.dirname(entry.path))
 
