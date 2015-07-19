93ba5de789ef39526383aa8328500e86effd037e
darumacity <lt012071@gmail.com>
2015-07-19 23:04:59 +0900

    [modify]Change styles

--- entry/urls.py
+++ entry/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from entry import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.top),
+    url(r'^$', views.top, name='top'),
     url(r'^media/(?P<media_path>.+)/$', views.media, name='media'),
     url(r'^(?P<entry_id>\d+)/$', views.content, name='content'),
     url(r'^(?P<entry_id>\d+)/(?P<resource_path>.+)/$', views.resource),
